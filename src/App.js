b93d7ad1c80847920447ce73278894bb76077727
duonghongduc2k4 <duc2022004@gmail.com>
2024-07-07 22:45:33 +0700

    b

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import { Route, Routes } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Router, Routes } from "react-router-dom"
+
 import Login from './components/Login';
+
 function App() {
     return (
         <>
             <Routes>
                 
-                <Route path='/' element={< Login />}></Route>
+                <Router path='/' element={< Login />}></Router>
            
             </Routes>
         </>
