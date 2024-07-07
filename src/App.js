3d74e6313c695edc484fbf7b6bb9848a46db4f07
duonghongduc2k4 <duc2022004@gmail.com>
2024-07-07 22:21:42 +0700

    test1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Route, Router, Routes } from "react-router-dom"
+import Home from './components/home';
+import Create from './components/Create';
+import Edit from './components/Edit';
+import Detail from "./components/detail";
+import HostList from "./components/host";
+import Login from './components/Login';
+import History from './components/History';
+import Confirm from "./components/confirm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <>
+            <Routes>
+                <Route path='/home' element={<Home />}></Route>
+                <Route path='/create' element={<Create />}></Route>
+                <Route path='/edit/:id' element={<Edit />}></Route>
+                <Route path='/detail/:id' element={<Detail />}></Route>
+                <Route path='/host' element={< HostList />}></Route>
+                <Route path='/' element={< Login />}></Route>
+                <Route path='/history/:id' element={<History />}></Route>
+                <Route path='/order/:id' element={<Confirm />}></Route>
 
+            </Routes>
+        </>
+    );
+}
 export default App;
